dc855346082d914ecbd7bb93f5186e385e3399b1
suraj <surajrathodofficial@gmail.com>
2022-09-29 13:23:14 +0530

    First commit

---
+++ 3datatypes.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int a;
+    cout<<"The size of int is " <<sizeof(a)<<" bytes"<<endl;
+
+    float b;
+     cout<<"The size of float is "<<sizeof(b)<<" bytes"<<endl;
+
+     char c;
+          cout<<"The size of character is "<<sizeof(c)<<" bytes"<<endl;
+
+bool d;
+     cout<<"The size of boolean is " <<sizeof(d)<<" bytes"<<endl;
+
+long int li;
+short int si;
+     cout<<"The size of long int is " <<sizeof(li)<<" bytes"<<endl;
+     cout<<"The size of short int is " <<sizeof(si)<<" bytes"<<endl;
+
+
+
+
+
+    return 0;
+}
---
+++ 9operators.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    int a,b;
+    cout<<"Enter two numbers"<<endl;
+    cin>>a>>b;
+
+    return 0;
+}
